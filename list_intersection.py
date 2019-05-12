a13687a803675c69fc92eac222ee881016834c7a
adityakhannaadk <38283382+adityakhannaadk@users.noreply.github.com>
2019-05-12 19:55:32 +0100

    Delete list_intersection.py

--- list_intersection.py
+++
@@ -1,9 +0,0 @@
-#Aditya Khanna @adityakhannaadk
-#An incredibly simple string comparison method.
-#Returns a positive integer.
-
-def compare(a,b):
-  lista = a.lower.split(" ")
-  listb = b.lower.split(" ")
-  return len(list(set(lista).intersection(listb)))
-       
